c5bde690ec4e4c8673f354b1d7c83218f75b7546
Bianca Rosa <me@biancarosa.com.br>
2016-12-01 00:53:43 -0200

    Adiciona rota para criar uma rede de classificação

--- src/app.py
+++ src/app.py
@@ -1,6 +1,25 @@
+import json
+
 from flask import Flask
+
+from sklearn.neural_network import MLPClassifier
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/classification')
+def classification():
+    X = [[0., 0., 0.], [1., 1., 1.]]
+    y = [0, 1]
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5,
+            hidden_layer_sizes=(5, 3), random_state=1)
+    clf.fit(X, y)
+    predictions = clf.predict([[2., 2., 3.], [-1., -2., -2.]])
+    response = { 'data' : [], 'success': False }
+    response['data'] = predictions.tolist()
+    response['success'] = True
+    data = json.dumps(response, indent=2)
+    return data
