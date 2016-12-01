8f121e1813aaa073effc24b5743de1bce5e01983
Bianca Rosa <me@biancarosa.com.br>
2016-12-01 14:07:03 -0200

    Parametriza as entries que geram o output da rede

--- api/routes/classification.py
+++ api/routes/classification.py
@@ -1,9 +1,22 @@
 from api import app
 from api.helpers.json_response_builder import JSONResponseBuilder
 from api.models import ClassifierNetwork
+from flask import request
 
-@app.route('/classification')
+@app.route('/classification/predict', methods=['POST'])
 def classification():
-    classifier_network = ClassifierNetwork()
-    predictions = classifier_network.predict([[1.,1.,1.], [0.,0.,0.]])
-    return JSONResponseBuilder.build_response(data=predictions)
+    post_data = request.get_json()
+
+    if post_data.get('entries'):
+        classifier_network = ClassifierNetwork()
+        predictions = classifier_network.predict(post_data['entries'])
+        return JSONResponseBuilder.build_response(
+            data={
+                "output" : predictions
+            }
+        ), 200
+    else:
+        return JSONResponseBuilder.build_response(
+                success=False,
+                messages=['Please send the entries to predict the output']
+            ), 400
