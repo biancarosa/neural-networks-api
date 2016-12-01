d08ff79fe70d7f7088c1c34222058d31d478a229
Bianca Rosa <me@biancarosa.com.br>
2016-12-01 14:18:22 -0200

    Add mongo configurations

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_mongoengine import MongoEngine
 
 app = Flask(__name__)
+app.config.from_pyfile('config.cfg')
+db = MongoEngine(app)
 
 import api.routes.hello_world
 import api.routes.classification
